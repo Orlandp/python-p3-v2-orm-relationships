77955a72bcf0982849727b2a4363782e175ed211
pknowledge <jamesblssings@gmail.com>
2025-08-22 14:37:31 +0300

    orm relationships

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-
-from __init__ import CONN, CURSOR
 from department import Department
 from employee import Employee
-import ipdb
-
 
 def reset_database():
     Employee.drop_table()
@@ -12,16 +8,20 @@ def reset_database():
     Department.create_table()
     Employee.create_table()
 
-    # Create seed data
+def seed():
     payroll = Department.create("Payroll", "Building A, 5th Floor")
-    human_resources = Department.create(
-        "Human Resources", "Building C, East Wing")
+    human_resources = Department.create("Human Resources", "Building C, East Wing")
     Employee.create("Amir", "Accountant", payroll.id)
     Employee.create("Bola", "Manager", payroll.id)
     Employee.create("Charlie", "Manager", human_resources.id)
     Employee.create("Dani", "Benefits Coordinator", human_resources.id)
     Employee.create("Hao", "New Hires Coordinator", human_resources.id)
 
-
-reset_database()
-ipdb.set_trace()
+if __name__ == "__main__":
+    reset_database()
+    seed()
+    try:
+        import ipdb; ipdb.set_trace()
+    except Exception:
+        print("Departments:", Department.get_all())
+        print("Employees:", Employee.get_all())
